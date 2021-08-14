c9bf87373ebd727753ded754e24eb2e17a721b77
MohamedFaruk_M <msmohamedfaruk@gmail.com>
2021-08-14 20:44:01 +0530

    remove TODOs

--- src/com/nisfa/main/ApplicationMain.java
+++ src/com/nisfa/main/ApplicationMain.java
@@ -13,7 +13,6 @@ public class ApplicationMain {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		
 		System.out.println("Happy Learning");
 		
