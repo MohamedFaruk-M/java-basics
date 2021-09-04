9f26ca555fcbad0727a6a8a6ebe4f327e9c9032d
MohamedFaruk_M <msmohamedfaruk@gmail.com>
2021-09-04 23:14:30 +0530

    impl builder design pattern

---
+++ src/com/nisfa/builder/StudentBuilder.java
@@ -0,0 +1,49 @@
+
+package com.nisfa.builder;
+
+import com.nisfa.main.Student;
+
+public class StudentBuilder {
+	
+	private int id;
+	private String name;
+	
+	private char gender;
+	private byte age;
+	
+	private long phoneNumber;
+	
+	
+	public static StudentBuilder getInstance(){
+		return new StudentBuilder();
+	}
+	
+	public StudentBuilder withId(int id) {
+		this.id = id;
+		return this;
+	}
+	
+	public StudentBuilder withName(String name) {
+		this.name = name;
+		return this;
+	}
+	
+	public StudentBuilder withGender(char gender) {
+		this.gender = gender;
+		return this;
+	}
+	
+	public StudentBuilder withAge(byte age) {
+		this.age = age;
+		return this;
+	}
+	
+	public StudentBuilder withPhoneNumber(long phoneNumber) {
+		this.phoneNumber = phoneNumber;
+		return this;
+	}
+	
+	public Student build() {
+		return new Student(id, name, gender, age, phoneNumber);
+	}
+}
--- src/com/nisfa/main/ApplicationMain.java
+++ src/com/nisfa/main/ApplicationMain.java
@@ -3,6 +3,8 @@
  */
 package com.nisfa.main;
 
+import com.nisfa.builder.StudentBuilder;
+
 /**
  * @author NisfaYasam
  *
@@ -13,11 +15,32 @@ public class ApplicationMain {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		
+
 		System.out.println("Happy Learning");
-		
+
 		System.out.println("Stay Healthy & Be Optimist");
-		
+
+		objects();
+
+	}
+
+	static void objects() {
+		System.out.println("---------Play around with Objects---------");
+
+		System.out.println("\nInit Student:\n");
+
+		Student student = StudentBuilder.getInstance().withId(1).withName("Rahul").withGender('M').withAge((byte) 27)
+				.withPhoneNumber(999_444_7281L).build();
+
+		System.out.println("\nExploring Student #1:\n");
+
+		System.out.println("id: " + student.getId());
+		System.out.println("name: " + student.getName());
+		System.out.println("gender: " + student.getGender());
+		System.out.println("age: " + student.getAge());
+		System.out.println("phone: " + student.getPhoneNumber());
+
+		System.out.println("\nExplored Student #1:\n");
 
 	}
 
