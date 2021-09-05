e75fc2b20ea3c210e735637741f2dfa5253ddb9b
MohamedFaruk_M <msmohamedfaruk@gmail.com>
2021-09-05 20:43:02 +0530

    refactor model with Serializable

---
+++ src/com/nisfa/model/Student.java
@@ -0,0 +1,70 @@
+
+package com.nisfa.model;
+
+import java.io.Serializable;
+
+public class Student implements Serializable {
+
+	private static final long serialVersionUID = 524002403937674871L;
+
+	private int id;
+	private String name;
+
+	private char gender;
+	private byte age;
+
+	private long phoneNumber;
+
+	public Student() {
+	}
+
+	public Student(int id, String name, char gender, byte age, long phoneNumber) {
+		this.id = id;
+		this.name = name;
+		this.gender = gender;
+		this.age = age;
+		this.phoneNumber = phoneNumber;
+	}
+
+	// commented after implemented student builder
+	/*
+	 * public static Student builder(){ return new Student(); }
+	 * 
+	 * public void withId(int id) { this.id = id; }
+	 * 
+	 * public void withName(String name) { this.name = name; }
+	 * 
+	 * public void withGender(char gender) { this.gender = gender; }
+	 * 
+	 * public void withAge(byte age) { this.age = age; }
+	 * 
+	 * public void withPhoneNumber(long phoneNumber) { this.phoneNumber =
+	 * phoneNumber; }
+	 */
+
+	public int getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public char getGender() {
+		return gender;
+	}
+
+	public byte getAge() {
+		return age;
+	}
+
+	public long getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	@Override
+	public String toString() {
+		return "Student [id=" + id + ", name=" + name + ", gender=" + gender + ", age=" + age + ", phoneNumber="
+				+ phoneNumber + "]";
+	}
+}
