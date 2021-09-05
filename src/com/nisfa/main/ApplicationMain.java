7e245eada83ba044589c7923d7a7aa91f16b1c98
MohamedFaruk_M <msmohamedfaruk@gmail.com>
2021-09-05 16:18:04 +0530

    various way of objectArray initialization

--- src/com/nisfa/main/ApplicationMain.java
+++ src/com/nisfa/main/ApplicationMain.java
@@ -14,13 +14,18 @@ public class ApplicationMain {
 	/**
 	 * @param args
 	 */
+
+	static int studentCount = 0;
+
 	public static void main(String[] args) {
 
 		System.out.println("Happy Learning");
 
 		System.out.println("Stay Healthy & Be Optimist");
 
-		objects();
+//		objects();
+
+		objectArray();
 
 	}
 
@@ -32,15 +37,78 @@ public class ApplicationMain {
 		Student student = StudentBuilder.getInstance().withId(1).withName("Rahul").withGender('M').withAge((byte) 27)
 				.withPhoneNumber(999_444_7281L).build();
 
-		System.out.println("\nExploring Student #1:\n");
+		printStudentDetails(student);
 
-		System.out.println("id: " + student.getId());
-		System.out.println("name: " + student.getName());
-		System.out.println("gender: " + student.getGender());
-		System.out.println("age: " + student.getAge());
-		System.out.println("phone: " + student.getPhoneNumber());
+//		System.out.println("\nExploring Student #1:\n");
+//
+//		System.out.println("id: " + student.getId());
+//		System.out.println("name: " + student.getName());
+//		System.out.println("gender: " + student.getGender());
+//		System.out.println("age: " + student.getAge());
+//		System.out.println("phone: " + student.getPhoneNumber());
+//
+//		System.out.println("\nExplored Student #1:\n");
 
-		System.out.println("\nExplored Student #1:\n");
+	}
+
+	static void objectArray() {
+		System.out.println("---------Lets explore object array----------");
+
+		// case 1: init with fixed length
+//		System.out.println("#### init with fixed length ####");
+//		Student[] students = new Student[4];
+
+//		students[0] = StudentBuilder.getInstance().withId(++studentCount).withName("Ravi").withGender('M')
+//				.withAge((byte) 32).withPhoneNumber(999_444_7283L).build();
+//		students[1] = StudentBuilder.getInstance().withId(++studentCount).withName("Rathinagiri").withGender('M')
+//				.withAge((byte) 28).withPhoneNumber(999_444_7284L).build();
+//		students[2] = StudentBuilder.getInstance().withId(++studentCount).withName("Subiksha").withGender('F')
+//				.withAge((byte) 26).withPhoneNumber(999_444_7285L).build();
+//		students[3] = StudentBuilder.getInstance().withId(++studentCount).withName("Roshini").withGender('F')
+//				.withAge((byte) 35).withPhoneNumber(999_444_7286L).build();
+
+		// case 2: init with fixed length along with instantiation
+//		System.out.println("#### init with fixed length along with instantiation ####");
+//		Student[] students = new Student[] {
+//				StudentBuilder.getInstance().withId(++studentCount).withName("Ravi").withGender('M').withAge((byte) 32)
+//						.withPhoneNumber(999_444_7283L).build(),
+//				StudentBuilder.getInstance().withId(++studentCount).withName("Rathinagiri").withGender('M')
+//						.withAge((byte) 28).withPhoneNumber(999_444_7284L).build(),
+//				StudentBuilder.getInstance().withId(++studentCount).withName("Subiksha").withGender('F')
+//						.withAge((byte) 26).withPhoneNumber(999_444_7285L).build(),
+//				StudentBuilder.getInstance().withId(++studentCount).withName("Roshini").withGender('F')
+//						.withAge((byte) 35).withPhoneNumber(999_444_7286L).build() };
+
+		// case 3 : ease of initialization w/o new operator
+		System.out.println("#### ease of initialization w/o new operator ####");
+
+		Student[] students = {
+				StudentBuilder.getInstance().withId(++studentCount).withName("Ravi").withGender('M').withAge((byte) 32)
+						.withPhoneNumber(999_444_7283L).build(),
+				StudentBuilder.getInstance().withId(++studentCount).withName("Rathinagiri").withGender('M')
+						.withAge((byte) 28).withPhoneNumber(999_444_7284L).build(),
+				StudentBuilder.getInstance().withId(++studentCount).withName("Subiksha").withGender('F')
+						.withAge((byte) 26).withPhoneNumber(999_444_7285L).build(),
+				StudentBuilder.getInstance().withId(++studentCount).withName("Roshini").withGender('F')
+						.withAge((byte) 35).withPhoneNumber(999_444_7286L).build() };
+
+		for (Student s : students) {
+			printStudentDetails(s);
+		}
+
+	}
+
+	static void printStudentDetails(Student student) {
+		System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+		System.out.println("\nExploring Student #" + student.getId() + ":\n");
+
+		System.out.println("id\t:" + student.getId());
+		System.out.println("name\t:" + student.getName());
+		System.out.println("gender\t:" + student.getGender());
+		System.out.println("age\t:" + student.getAge());
+		System.out.println("phone\t:" + student.getPhoneNumber());
+
+		System.out.println("\nExplored Student #" + student.getId() + "\n");
 
 	}
 
