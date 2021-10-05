923eab923b72090f4e23390ab9282869bcf2a348
MohamedFaruk_M <msmohamedfaruk@gmail.com>
2021-10-05 22:34:08 +0530

    added template for hands-on-learning

---
+++ src/com/nisfa/demo/LrnFinalAndConstant.java
@@ -0,0 +1,13 @@
+package com.nisfa.demo;
+
+public class LrnFinalAndConstant {
+	
+	// final keyword uniqueness
+	
+	// Constant as a type of final
+	
+	// final but not constant
+	
+	// case study: switch case
+
+}
